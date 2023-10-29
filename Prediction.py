b26510f2be3ec08e432306ba3c64901fbc7883cf
Edalet-eng <118162083+Edalet-eng@users.noreply.github.com>
2023-10-30 00:38:48 +0400

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -305,7 +305,14 @@ with interface:
         cursor.execute("INSERT INTO yorumlar (yorum) VALUES (?)", (yorum,))
         conn.commit()
         st.success("Yorumunuz uğurla əlavə edildi.")
+    st.title('Yorumlar')
+    cursor.execute("SELECT yorum FROM yorumlar")
+    yorumlar = cursor.fetchall()
+    for yorum in yorumlar:
+        st.write(yorum[0])
     
+    # Veritabanı bağlantısını kapat
+    conn.close()
     
    
       
