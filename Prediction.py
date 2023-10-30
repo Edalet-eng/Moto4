28384276c2ff8a3b08baa11a1c9950807217e462
Edalet-eng <118162083+Edalet-eng@users.noreply.github.com>
2023-10-30 21:20:12 +0400

    Update Prediction.py

--- Prediction.py
+++ Prediction.py
@@ -303,6 +303,21 @@ with interface:
         cursor.execute("INSERT INTO yorumlar (yorum) VALUES (?)", (yorum,))
         conn.commit()
         st.success("Yorumunuz uğurla əlavə edildi.")
+   import sqlite3
+
+   # Yorumlar bazasına qoşulun
+   conn = sqlite3.connect('yorumlar.db')
+   cursor = conn.cursor()
    
-      
+   # SQL sorğusu ilə yorumları əldə edin
+   cursor.execute("SELECT * FROM yorumlar")
+   yorumlar = cursor.fetchall()
+   
+   # Yorumlar bazası ilə işiniz bitdikdə əlaqəni bağlayın
+   conn.close()
+   
+   # Əldə etdiyiniz yorumları görüntülə
+   for yorum in yorumlar:
+       st.write(yorum)
+         
       
