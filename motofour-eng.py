0861645d2dee5bad7320101a9a35499dc6039edc
Kamal Agayev <95571204+AgayevKamal@users.noreply.github.com>
2023-12-06 22:40:35 +0400

    Update motofour-eng.py
    Signed-off-by: Kamal Agayev <95571204+AgayevKamal@users.noreply.github.com>

--- motofour-eng.py
+++ motofour-eng.py
@@ -1,35 +1,23 @@
 from PIL import Image
 import streamlit as st
-import sklearn
+import openai
 import pandas as pd
 import pickle
 import numpy as np
+import sqlite3
 import time
 from sklearn.preprocessing import LabelEncoder
-import sqlite3
-import openai
-
+import sklearn
+import streamlit as st
+from sqlalchemy import create_engine, Column, Integer, String, Float, MetaData, Table
+from sqlalchemy.orm import declarative_base, Session
 st.image('587-161.png', use_column_width=True)
-df=pd.read_csv('data.csv')
+
+df=pd.read_csv('lastdata.csv')
  
-def label_encoder_process(data_frame=None):
-    for i in data_frame.columns:
-        lb = LabelEncoder()
-        data_frame[i]=lb.fit_transform(data_frame[i])
-    return data_frame
+
     
 interface = st.container()
-
-
-st.sidebar.header('Get more information about you were interested car!')
-
-
-
-
-
-
-
-
 
 
 with interface:
@@ -203,12 +191,18 @@ with interface:
         
         
     st.write('<hr style="height: px; background-color: gray; border: none; margin: px 0;" />', unsafe_allow_html=True)
+    
+    
+    
+    elave_melumat = st.text_area("Write additional notes about your car")
+    
+    st.write('<hr style="height: px; background-color: gray; border: none; margin: px 0;" />', unsafe_allow_html=True)
 
     
 
     st.sidebar.title("Your guide")
     
-    openai.api_key = "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"
+    openai.api_key = "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"
     
     if "openai_model" not in st.session_state:
         st.session_state["openai_model"] = "gpt-3.5-turbo"
@@ -223,7 +217,7 @@ with interface:
     
         prompt = st.text_input("Say something:", key="user_input")
         submit_button = st.form_submit_button("Enter")
-    
+        car_info_button = st.sidebar.button("Get general information about the entered car")
     if submit_button:
         st.session_state.messages.append({"role": "user", "content": prompt})
     
@@ -247,7 +241,7 @@ with interface:
         st.session_state.messages.append({"role": "assistant", "content": full_response})
     
     
-    if st.sidebar.button("Get more information!"):
+    if car_info_button:
         # Get the selected values for marka and model
         marka_value = marka
         model_value = model
@@ -277,7 +271,11 @@ with interface:
                 message_placeholder.markdown(full_response + "▌")
             message_placeholder.markdown(full_response)
         st.session_state.messages.append({"role": "assistant", "content": full_response})
-
+        
+        
+        
+        
+        
     rənglənib_encoding = {'rənglənməyib':1,'rənglənib':0}
     vuruğu_var_encoding = {'vuruğu yoxdur':1,'vuruğu var':0}
        
@@ -295,58 +293,58 @@ with interface:
     
 
 
-    marka = marka_mapping[marka]
-    model = model_mapping[model]
-    yanacaq_novu = yanacaq_novu_mapping[yanacaq_novu]
-    ötürücü = ötürücü_mapping[ötürücü]
-    ban_növü = ban_növü_mapping[ban_növü]
-    sürətlər_qutusu = sürətlər_qutusu_mapping[sürətlər_qutusu]
-    rəng = rəng_mapping[rəng]
-    hansı_bazar_üçün_yığılıb = hansı_bazar_mapping[hansı_bazar_üçün_yığılıb]
-    rənglənib = rənglənib_encoding[rənglənib]
-    vuruğu_var = vuruğu_var_encoding[vuruğu_var]
+    marka2 = marka_mapping[marka]
+    model2 = model_mapping[model]
+    yanacaq_novu2 = yanacaq_novu_mapping[yanacaq_novu]
+    ötürücü2 = ötürücü_mapping[ötürücü]
+    ban_növü2 = ban_növü_mapping[ban_növü]
+    sürətlər_qutusu2 = sürətlər_qutusu_mapping[sürətlər_qutusu]
+    rəng2 = rəng_mapping[rəng]
+    hansı_bazar_üçün_yığılıb2 = hansı_bazar_mapping[hansı_bazar_üçün_yığılıb]
+    rənglənib2 = rənglənib_encoding[rənglənib]
+    vuruğu_var2 = vuruğu_var_encoding[vuruğu_var]
     
-    lehimli_disk = int(lehimli_disk)
-    abs = int(abs)
-    lyuk = int(lyuk)
-    yağış_sensoru = int(yağış_sensoru)
-    mərkəzi_qapanma = int(mərkəzi_qapanma)
-    park_radarı = int(park_radarı)
-    kondisioner = int(kondisioner)
-    oturacaqların_isidilməsi = int(oturacaqların_isidilməsi)
-    dəri_salon = int(dəri_salon)
-    ksenon_lampalar = int(ksenon_lampalar)
-    arxa_görüntü_kamerası = int(arxa_görüntü_kamerası)
-    yan_pərdələr = int(yan_pərdələr)
-    oturacaqların_ventilyasiyası = int(oturacaqların_ventilyasiyası)
+    lehimli_disk2 = int(lehimli_disk)
+    abs2 = int(abs)
+    lyuk2 = int(lyuk)
+    yağış_sensoru2 = int(yağış_sensoru)
+    mərkəzi_qapanma2 = int(mərkəzi_qapanma)
+    park_radarı2 = int(park_radarı)
+    kondisioner2 = int(kondisioner)
+    oturacaqların_isidilməsi2 = int(oturacaqların_isidilməsi)
+    dəri_salon2 = int(dəri_salon)
+    ksenon_lampalar2 = int(ksenon_lampalar)
+    arxa_görüntü_kamerası2 = int(arxa_görüntü_kamerası)
+    yan_pərdələr2 = int(yan_pərdələr)
+    oturacaqların_ventilyasiyası2 = int(oturacaqların_ventilyasiyası)
 
        
 
     
     input_features = pd.DataFrame({
-        'marka': [marka],
-        'model': [model],
-        'ban_növü': [ban_növü],
-        'rəng': [rəng],
-        'sürətlər_qutusu': [sürətlər_qutusu],
-        'ötürücü': [ötürücü],
-        'hansı_bazar_üçün_yığılıb': [hansı_bazar_üçün_yığılıb],
-        'yanacaq_novu': [yanacaq_novu],
-        'vuruğu_var': [vuruğu_var],
-        'rənglənib': [rənglənib],
-        'lehimli_disk': [lehimli_disk],
-        'abs': [abs],
-        'lyuk': [lyuk],
-        'yağış_sensoru': [yağış_sensoru],
-        'mərkəzi_qapanma': [mərkəzi_qapanma],
-        'park_radarı': [park_radarı],
-        'kondisioner': [kondisioner],
-        'oturacaqların_isidilməsi': [oturacaqların_isidilməsi],
-        'dəri_salon': [dəri_salon],
-        'ksenon_lampalar': [ksenon_lampalar],
-        'arxa_görüntü_kamerası': [arxa_görüntü_kamerası],
-        'yan_pərdələr': [yan_pərdələr],
-        'oturacaqların_ventilyasiyası': [oturacaqların_ventilyasiyası],
+        'marka': [marka2],
+        'model': [model2],
+        'ban_növü': [ban_növü2],
+        'rəng': [rəng2],
+        'sürətlər_qutusu': [sürətlər_qutusu2],
+        'ötürücü': [ötürücü2],
+        'hansı_bazar_üçün_yığılıb': [hansı_bazar_üçün_yığılıb2],
+        'yanacaq_novu': [yanacaq_novu2],
+        'vuruğu_var': [vuruğu_var2],
+        'rənglənib': [rənglənib2],
+        'lehimli_disk': [lehimli_disk2],
+        'abs': [abs2],
+        'lyuk': [lyuk2],
+        'yağış_sensoru': [yağış_sensoru2],
+        'mərkəzi_qapanma': [mərkəzi_qapanma2],
+        'park_radarı': [park_radarı2],
+        'kondisioner': [kondisioner2],
+        'oturacaqların_isidilməsi': [oturacaqların_isidilməsi2],
+        'dəri_salon': [dəri_salon2],
+        'ksenon_lampalar': [ksenon_lampalar2],
+        'arxa_görüntü_kamerası': [arxa_görüntü_kamerası2],
+        'yan_pərdələr': [yan_pərdələr2],
+        'oturacaqların_ventilyasiyası': [oturacaqların_ventilyasiyası2],
         'buraxılış_ili': [buraxılış_ili],
         'yürüş': [yürüş],
         'mühərrik_hecmi': [mühərrik_hecmi],
@@ -354,57 +352,184 @@ with interface:
         
     })
     
-    
+    # SQLite veritabanı ilə əlaqə yaratmaq
+    engine = create_engine('sqlite:///cars.db', echo=True)
 
-    st.subheader(body = 'Model Prediction')
-    
-    with open('saved_model.pickle', 'rb') as pickled_model:
+    # Modeli təyin etmək
+    Base = declarative_base()
+    class Car(Base):
+        __tablename__ = 'cars'
+        id = Column(Integer, primary_key=True)
+        marka = Column(String)
+        model = Column(String)
+        yanacaq_novu = Column(String)
+        ötürücü = Column(String)
+        ban_növü = Column(String)
+        sürətlər_qutusu = Column(String)
+        yürüş = Column(Integer)
+        buraxılış_ili = Column(Integer)
+        rəng = Column(String)
+        hansı_bazar_üçün_yığılıb = Column(String)
+        mühərrik_hecmi = Column(Integer)
+        mühərrik_gucu = Column(Integer)
+        rənglənib = Column(String)
+        vuruğu_var = Column(String)
+        lehimli_disk = Column(String)
+        abs = Column(String)
+        lyuk = Column(String)
+        yağış_sensoru = Column(String)
+        dəri_salon = Column(String)
+        mərkəzi_qapanma = Column(String)
+        park_radarı = Column(String)
+        kondisioner = Column(String)
+        oturacaqların_isidilməsi = Column(String)
+        ksenon_lampalar = Column(String)
+        arxa_görüntü_kamerası = Column(String)
+        yan_pərdələr = Column(String)
+        oturacaqların_ventilyasiyası = Column(String)
+        elave_melumat = Column(String)
+        qiymet = Column(Float)
         
-        model = pickle.load(pickled_model)
+
+    # Veritabanını yaratmaq üçün
+    metadata = MetaData()
+    cars_table = Table('cars', metadata,
+                       Column('id', Integer, primary_key=True),
+                       Column('marka', String),
+                       Column('model', String),
+                       Column('yanacaq_novu', String),
+                       Column('ötürücü', String),
+                       Column('ban_növü', String),
+                       Column('sürətlər_qutusu', String),
+                       Column('yürüş', Integer),
+                       Column('buraxılış_ili', Integer),
+                       Column('rəng', String),
+                       Column('hansı_bazar_üçün_yığılıb', String),
+                       Column('mühərrik_hecmi', Integer),
+                       Column('mühərrik_gucu', Integer),
+                       Column('rənglənib', String),
+                       Column('vuruğu_var', String),
+                       Column('lehimli_disk', String),
+                       Column('abs', String),
+                       Column('lyuk', String),
+                       Column('yağış_sensoru', String),
+                       Column('dəri_salon', String),
+                       Column('mərkəzi_qapanma', String),
+                       Column('park_radarı', String),
+                       Column('kondisioner', String),
+                       Column('oturacaqların_isidilməsi', String),
+                       Column('ksenon_lampalar', String),
+                       Column('arxa_görüntü_kamerası', String),
+                       Column('yan_pərdələr', String),
+                       Column('oturacaqların_ventilyasiyası', String),
+                       Column('elave_melumat' , String),
+                       Column('qiymet', Float))
+    metadata.create_all(engine)
+
+    st.subheader(body = 'Model prediction')
     
-    if st.button('Predict'):
-        cars_price = model.predict(input_features)
 
-        with st.spinner('Sending input features to model...'):
-            time.sleep(2)
+    try:
+        with open('saved_model.pickle', 'rb') as pickled_model:
+            pred_model = pickle.load(pickled_model)
+    except Exception as e:
+        st.error(f"Something wrong: {e}")
+        # Add more details or actions if necessary
 
-        st.success('Prediction is ready')
-        time.sleep(1)
-        st.markdown(f'### Car\'s estimated price is:  {cars_price} AZN')
+        
+        
+    button1,button2=st.columns(2)
+    if button1.button('Predict'):
+        try:
+            if df[df['model'] == model_mapping[model]]['model'].count() < 10:
+                st.warning("The car price you enter may not be predictable because there is not enough information in the database")
+            else:
+                st.success('Hesablanır')
+                time.sleep(1)
+                st.markdown(f'### Estimated price for the car: {np.round(int(pred_model.predict(input_features)),-2)} AZN')
+        except Exception as e:
+            st.error(f"Something wrong: {e}")
+    # Add more details or actions if necessary
+
     
     st.write('<hr style="height: px; background-color: gray; border: none; margin: px 0;" />', unsafe_allow_html=True)
-    # Yorumlar bazasına qoşulun
-    conn = sqlite3.connect('yorumlar.db')
-    cursor = conn.cursor()
+    qiymet = np.round(int(pred_model.predict(input_features)),-2)    
+
+    # Streamlit tətbiqindən gələn məlumatları veritabanına əlavə etmək üçün funksiya
+    def elan_əlavə_et(marka, model, yanacaq_novu, ötürücü, ban_növü, sürətlər_qutusu, yürüş, buraxılış_ili, rəng, hansı_bazar_üçün_yığılıb, mühərrik_hecmi, mühərrik_gucu, rənglənib, vuruğu_var, lehimli_disk, abs, lyuk, yağış_sensoru, dəri_salon, mərkəzi_qapanma, park_radarı, kondisioner, oturacaqların_isidilməsi, ksenon_lampalar, arxa_görüntü_kamerası, yan_pərdələr, oturacaqların_ventilyasiyası,elave_melumat,qiymet):
+        new_car = Car(marka=marka, model=model, yanacaq_novu=yanacaq_novu, ötürücü=ötürücü, ban_növü=ban_növü, sürətlər_qutusu=sürətlər_qutusu, yürüş=yürüş, buraxılış_ili=buraxılış_ili, rəng=rəng, hansı_bazar_üçün_yığılıb=hansı_bazar_üçün_yığılıb, mühərrik_hecmi=mühərrik_hecmi, mühərrik_gucu=mühərrik_gucu, rənglənib=rənglənib, vuruğu_var=vuruğu_var, lehimli_disk=lehimli_disk, abs=abs, lyuk=lyuk, yağış_sensoru=yağış_sensoru, dəri_salon=dəri_salon, mərkəzi_qapanma=mərkəzi_qapanma, park_radarı=park_radarı, kondisioner=kondisioner, oturacaqların_isidilməsi=oturacaqların_isidilməsi, ksenon_lampalar=ksenon_lampalar, arxa_görüntü_kamerası=arxa_görüntü_kamerası, yan_pərdələr= yan_pərdələr, oturacaqların_ventilyasiyası=oturacaqların_ventilyasiyası,elave_melumat=elave_melumat,qiymet=qiymet)
+        session = Session(bind=engine)
+        session.add(new_car)
+        session.commit ()
+        session.close()
+    # Streamlit tətbiqindən gələn məlumatlarla əlavə etmə funksiyasını çağırmaq
+    if button2.button("Add announment"):
+        try:
+            if df[df['model'] == model_mapping[model]]['model'].count() < 10:
+                st.warning("Your announcment may not be added because the price cannot be predicted.")
+            else:
+                elan_əlavə_et(marka, model, yanacaq_novu, ötürücü, ban_növü, sürətlər_qutusu, yürüş, buraxılış_ili, rəng, hansı_bazar_üçün_yığılıb, mühərrik_hecmi, mühərrik_gucu, rənglənib, vuruğu_var, lehimli_disk, abs, lyuk, yağış_sensoru, dəri_salon, mərkəzi_qapanma, park_radarı, kondisioner, oturacaqların_isidilməsi, ksenon_lampalar, arxa_görüntü_kamerası, yan_pərdələr, oturacaqların_ventilyasiyası,elave_melumat,qiymet)
+                st.success("Announcement added!")
+                
+        except Exception as e:
+            st.error(f"Something wrong: {e}")
+        
+
+
     
-    # Streamlit tətbiqini yaradın
-    st.subheader('Comments ')
     
-    # Yorum əlavə etmə formunu tərtib edin
-    yorum = st.text_area("Enter your comment:")
-    submit = st.button("Send")
     
-    # Yorum göndərildikdə
-    if submit:
-        # Əlavə olunacaq yorumları bazaya yazın
-        cursor.execute("INSERT INTO yorumlar (yorum) VALUES (?)", (yorum,))
-        conn.commit()
-        st.success("Yorumunuz uğurla əlavə edildi.")
-     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    # SQLite veritabanı ilə əlaqə yaratmaq
+    engine = create_engine('sqlite:///comment.db', echo=True)
+
+    # Modeli təyin etmək
+    Base = declarative_base()
+    class Comment(Base):
+        __tablename__ = 'comment'
+        id = Column(Integer, primary_key=True)
+        comment = Column(String)
+       
+
+    # Veritabanını yaratmaq üçün
+    metadata = MetaData()
+    cars_table = Table('comment', metadata,
+                       Column('id', Integer, primary_key=True),
+                       Column('comment', String))
+    metadata.create_all(engine)
+
    
-    # Yorumlar bazasına qoşulun
-   # conn = sqlite3.connect('yorumlar.db')
-    #cursor = conn.cursor()
-    
-    # SQL sorğusu ilə yorumları əldə edin
-    #cursor.execute("SELECT * FROM yorumlar")
-   # yorumlar = cursor.fetchall()
-    
-    # Yorumlar bazası ilə işiniz bitdikdə əlaqəni bağlayın
-    conn.close()
-    
-    
-         
-      
-    
-    
+    st.title('Comments')
+   
+    # Yorum əlavə etmə formunu tərtib edin
+    yorum = st.text_area("Add your comment:")
+    submit = st.button("Submit")
+   
+   
+    def yorum_elave_et(yorum):
+        new_comment = Comment(comment=yorum)
+        session = Session(bind=engine)
+        session.add(new_comment)
+        session.commit ()
+        session.close()
+
+
+    # Streamlit tətbiqindən gələn məlumatlarla əlavə etmə funksiyasını çağırmaq
+    # Streamlit tətbiqindən gələn məlumatlarla əlavə etmə funksiyasını çağırmaq
+    if submit:
+        yorum_elave_et(yorum)
+        st.success("Şərh əlavə edildi!")
+ 
+
+ 
