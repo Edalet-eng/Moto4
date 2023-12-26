bb1c6ea0b76e63388eb6e02e60d80aa987349dab
Edalet-eng <118162083+Edalet-eng@users.noreply.github.com>
2023-12-26 22:52:07 +0400

    Update moto4-az.py
    Signed-off-by: Edalet-eng <118162083+Edalet-eng@users.noreply.github.com>

--- moto4-az.py
+++ moto4-az.py
@@ -352,7 +352,7 @@ with interface:
         # Şəkili oxu
         image_data = uploaded_image.read()
         # Şəkili Deta verilənlər bazasına əlavə et
-        response = db_pic.put({"key": "unique_key", "sekil": image_data})
+        response = db_pic.put({"key": 1, "sekil": image_data})
         # İstifadəçiyə bildiriş ver
         st.success("Şəkil uğurla əlavə edildi.")
        
