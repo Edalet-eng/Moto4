2cab43527c80b656ee8165d74ff20b6978132afe
Edalet-eng <118162083+Edalet-eng@users.noreply.github.com>
2024-05-01 15:38:29 +0400

    Update moto4-az.py
    Signed-off-by: Edalet-eng <118162083+Edalet-eng@users.noreply.github.com>

--- moto4-az.py
+++ moto4-az.py
@@ -368,10 +368,9 @@ with interface:
         submit_button = st.form_submit_button("Daxil et")
         car_info_button = st.sidebar.button("Avtomobiliniz haqqında məlumat al")
     if submit_button:
-        prompt_eng = translator.translate(prompt, src='az', dest='en')
-      
+        prompt_eng = translator.translate(prompt, src='az', dest='en')
         st.session_state.messages.append({"role": "user", "content": prompt_eng.text})
-      
+
         with st.sidebar.chat_message("user"):
             st.markdown(prompt)
 
